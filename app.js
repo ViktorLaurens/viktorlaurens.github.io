3537f42f0592da28661e5a785cb241315b0a5122
viktor <viktor.degroote@gmail.com>
2024-10-20 18:08:01 +0200

    Update.

--- app.js
+++ app.js
@@ -38,4 +38,15 @@ document.addEventListener("DOMContentLoaded", function() {
     observer.observe(target);
 });
 
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
 
